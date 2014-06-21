b5b6aabc2feab7b66762fb02b1d0919e7ade1f49
Bryan Anderson <shadow431@gmail.com>
2014-06-20 20:10:50 -0700

    added comments to master.py

--- master.py
+++ master.py
@@ -1,19 +1,28 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask import request
 import yaml 
+
 app = Flask(__name__)
+
+#Get the list of pins from yaml file
 f = open('./pins.yaml','r')
 yamlarr = yaml.load(f.read())
+
+#return the pins for the request pi serial number
 @app.route("/getpins", methods=['GET'])
 def getpins():
     serialNum = request.args.get('serNum')
     return yaml.dump(yamlarr['pins'][serialNum])
 
+#recieve the status of the pins
 @app.route("/pinstatus",methods=['GET'])
 def pinStatus():
     serialNum = request.args.get('serNum')
     pin = request.args.get('pin')
     status = request.args.get('status')
     return "recieved" 
+
 if __name__ == "__main__":
    app.run() 
