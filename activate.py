6844cbe037ab599e5df94bf683434147b6e1ab09
Bryan Anderson <shadow431@gmail.com>
2014-06-20 23:04:28 -0700

    removed uneeded code from activate.py

--- activate.py
+++ activate.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import time
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
@@ -9,23 +8,7 @@ except RuntimeError:
 GPIO.setmode(GPIO.BOARD)
 
 #Warnings?
-#GPIO.setwarnings(True)
-
-pinStatus = {} 
-
-def getserial():
-  # Extract serial from cpuinfo file
-  cpuserial = "0000000000000000"
-  try:
-    f = open('/proc/cpuinfo','r')
-    for line in f:
-      if line[0:6]=='Serial':
-        cpuserial = line[10:26]
-    f.close()
-  except:
-    cpuserial = "ERROR000000000"
-
-  return cpuserial
+GPIO.setwarnings(False)
 
 def pinSetup(pin,type):
     if type == 'in':
@@ -33,17 +16,9 @@ def pinSetup(pin,type):
     elif type == 'out':
         GPIO.setup(pin, GPIO.OUT)
 
-def checkPin(pin):
-    return GPIO.input(pin)
-
-def getPinsFromHost():
-    return [12]
-
-def notifyHost(pin,status):
-    return str(pin)+": "+str(status) 
 
 if __name__ == '__main__':
-    pins = getPinsFromHost()
+    pins = [12]
     for pin in pins:
         pinSetup(pin,'out')
 
