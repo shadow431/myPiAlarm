2bdf3eb3d3d7e92590c2d8be14702629e87b3339
Bryan Anderson <shadow431@gmail.com>
2014-06-20 21:47:11 -0700

    added error handling, email notice, and retry logic if server is unvailable

--- alarmFunctions.py
+++ alarmFunctions.py
@@ -51,7 +51,14 @@ def getPinsFromHost(server):
 
 def notifyHost(pin,status,server):
     server = "http://"+server+"/pinstatus?pin="+str(pin)+"&status="+str(status)+"&serNum="+str(getSerial())
-    response = yaml.load(urllib2.urlopen(server))
+    while True:
+        try:
+            response = yaml.load(urllib2.urlopen(server))
+        except urllib2.URLError,e:
+            commonFunc.email("There was an error connecting to: "+server+"\nError:"+str(e))
+            time.sleep((30*60))
+            continue 
+        break
     return response
 
 def main():
