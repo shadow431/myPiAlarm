af6aab9ea284e62c633aab9b7a759b8f10e7dc0b
Bryan Anderson <shadow431@gmail.com>
2014-06-20 19:56:56 -0700

    pin setup, and check

--- alarmFunctions.py
+++ alarmFunctions.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error import RPi.GPIO!  Are you sudo?")
+
+#Set the Pin Numbering Mode: BOARD=the pin number on the board, BCM=the channel numbers
+GPIO.setmode(GPIO.BOARD)
+
+#Warnings?
+#GPIO.setwarnings(True)
+
+def pinSetup(pin,type):
+    if type == 'in':
+        GPIO.setup(pin, GPIO.IN)
+    elif type == 'out':
+        GPIO.setup(pin, GPIO.OUT)
 
 def checkPin(pin):
-
-    return "GRRR"
+    return GPIO.input(pin)
 
 if __name__ == '__main__':
-    print checkPin(1)
+    pin = 15
+    pinSetup(pin,'in')
+    print checkPin(15)
