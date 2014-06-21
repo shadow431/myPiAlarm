99b7aaf04563be338a613e13bdd4b9d0b6b5c950
Bryan Anderson <shadow431@gmail.com>
2014-06-20 20:36:44 -0700

    setup piCheck.py to check the status.yaml file for checkins and see if any are older then the allowable amount of time.

--- piCheck.py
+++ piCheck.py
@@ -1,3 +1,7 @@
 import time, commonFunc
-
-
+time = time.time()
+status = commonFunc.getYaml('status')
+settings = commonFunc.getYaml('settings')
+for pi in status['checkIn']:
+    if ((time - status['checkIn'][pi])/settings['checkinTime']) >= 1:
+        commonFunc.email('Pi: '+str(pi)+', hasn\'t checked in in the required amount of time')
