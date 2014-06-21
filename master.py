9787ac055771a7ccf9dab90a626fa4ebc9ee0e46
Bryan Anderson <shadow431@gmail.com>
2014-06-20 20:16:52 -0700

    add import of commonFunc module setup some new dicts and hashes for storing pin, and arm status configured master.py to read armed, and pin status from pins.yaml for persistant storage across reboots added use of the global allPins dict to getPins() renamed pinStatus() to recievePinStatus. updated recievePinStatus() to use setStatus() to store updates, also to check for armed status of zones the pin belongs to and trigger an email if armed. Added arm() to set the arming of zones Added isArmed() to check the armed status of zones added setStatus() to update the existing status dict and trigger a write to disk added writeStatus() to write status dicts (pins, armedZones) to disk

--- master.py
+++ master.py
@@ -2,27 +2,96 @@
 
 from flask import Flask
 from flask import request
-import yaml 
+import yaml, commonFunc
 
+armedZones = []
+tmpStatus = {}
+pinStatus = {}
 app = Flask(__name__)
 
 #Get the list of pins from yaml file
-f = open('./pins.yaml','r')
-yamlarr = yaml.load(f.read())
+pinYaml = open('./pins.yaml','r')
+yamlPins = yaml.load(pinYaml.read())
+allPins = yamlPins['pins']
+
+statusYaml = open('status.yaml','r')
+tmpStatus = yaml.load(statusYaml.read())
+if tmpStatus > 0:
+    try:
+        tmpStatus['pins']
+    except KeyError:
+        pinStatus = {}
+    else:
+        pinStatus = tmpStatus['pins']
+    
+    try:
+        tmpStatus['armed']
+    except KeyError:
+        armedZones = []
+    else:
+        armedZones = tmpStatus['armed']
+statusYaml.close()
 
 #return the pins for the request pi serial number
 @app.route("/getpins", methods=['GET'])
 def getpins():
+    global allPins
+
     serialNum = request.args.get('serNum')
-    return yaml.dump(yamlarr['pins'][serialNum])
+
+    return yaml.dump(allPins[serialNum])
 
 #recieve the status of the pins
 @app.route("/pinstatus",methods=['GET'])
-def pinStatus():
-    serialNum = request.args.get('serNum')
-    pin = request.args.get('pin')
-    status = request.args.get('status')
-    return "recieved" 
+def recievePinStatus():
+    global allPins
+    global pinStatus
+
+    serialNum = str(request.args.get('serNum'))
+    pin = int(request.args.get('pin'))
+    status = int(request.args.get('status'))
+    setStatus(serialNum,pin,status)
+    if isArmed(allPins[serialNum][pin]['zones'])  == True and status==1:
+        email = commonFunc.email('Pin: '+str(pin)+'\nStatus: '+str(status))
+    return "Ok" 
+
+#arm the alarm
+@app.route("/arm",methods=['GET'])
+def arm():
+    global armedZones
+    zone = str(request.args.get('zone'))
+    armedZones.append(zone)
+    writeStatus()
+    return "Armed Zone" 
+
+def isArmed(zones):
+    global armedZones
+
+    if len(armedZones) == 0:
+        return False 
+    for zone in zones:
+        if zone in armedZones:
+            return True
+    return False 
+
+def setStatus(serNum,pin,status):
+    global pinStatus
+    try:
+        pinStatus[serNum]
+    except KeyError:
+        pinStatus[serNum] = {}
+    pinStatus[serNum][pin] = status
+    writeStatus()
+    return
+
+def writeStatus():
+    global armedZones
+    global pinStatus
+
+    statusYaml = open('status.yaml','r+')
+    statusYaml.write(yaml.dump({'armed':armedZones,'pins':pinStatus}))
+    statusYaml.close
+    return
 
 if __name__ == "__main__":
    app.run(debug=True) 
