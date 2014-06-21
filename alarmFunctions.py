aa09acbf4a2afa3779c0e3466db8da37f8ca0266
Bryan Anderson <shadow431@gmail.com>
2014-06-20 19:59:47 -0700

    added a function getSerial() to pull for the serial number for /proc/cpuinfo and return it

--- alarmFunctions.py
+++ alarmFunctions.py
@@ -11,6 +11,22 @@ GPIO.setmode(GPIO.BOARD)
 #Warnings?
 #GPIO.setwarnings(True)
 
+pinStatus = {} 
+
+def getSerial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
 def pinSetup(pin,type):
     if type == 'in':
         GPIO.setup(pin, GPIO.IN)
