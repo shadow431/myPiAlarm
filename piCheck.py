1f05ce81dec069bbda3d392bd2d61e168334df46
Bryan Anderson <shadow431@gmail.com>
2014-08-14 23:42:54 +0000

    added some output to piCheck.py

--- piCheck.py
+++ piCheck.py
@@ -2,6 +2,9 @@ import time, commonFunc
 time = time.time()
 status = commonFunc.getYaml('status')
 settings = commonFunc.getYaml('settings')
+print "Checking"
 for pi in status['checkIn']:
+    print str(pi)+" checked"
     if ((time - status['checkIn'][pi])/settings['checkinTime']) >= 1:
+        print str(pi)+" is stale"
         commonFunc.email('Pi: '+str(pi)+', hasn\'t checked in in the required amount of time')
