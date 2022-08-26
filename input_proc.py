ccdb76f0c9feec33c789a2cd5c8c92b27e6327d6
Bryan Anderson <shadow431@gmail.com>
2022-08-26 11:16:08 -0700

    updated status diff in input_proc to set pin numbers based on expander board number

--- input_proc.py
+++ input_proc.py
@@ -7,12 +7,13 @@ class input_proc:
       bool_list.append(False)
     return bool_list
   
-  def diff_values(current_status, past_status):
+  def diff_values(current_status, past_status, board):
     diff = {}
+    boardNumbers = (board+1)*100
     i = 0
     while i < 16:
       if current_status[i] != past_status[i]:
-        diff[(i+1)] = current_status[i]
+        diff[(i+boardNumbers)] = current_status[i]
       i += 1
     return diff
 
